5192ca48ffd9de3c5233ac3e0675a9d0dd31c92f
Almaelado <borsny75@gmail.com>
2025-02-11 11:02:15 +0100

    vegleges1

--- index.js
+++ index.js
@@ -32,20 +32,45 @@ function kiir(asd){
         <p>${asd[i].description}</p>
         <p id="ar">${asd[i].price} Ft</p>
         `;
+        rendeles.push({name:asd[i].name,darab:0});
         let gomb = document.createElement("button");
         gomb.textContent="Rendelés";
         gomb.addEventListener('click',x=>{
-            rendeles.push(String(asd[i].name));
-            alert(`A ${asd[i].name} hozzáadtuk a rendelésedhez!`);
+            for(let j = 0;j<rendeles.length;j++){
+                if(rendeles[j].name==asd[i].name){
+                    rendeles[j].darab++;
+                    kosar_fris();
+                    alert(`A ${asd[i].name} hozzáadtuk a rendelésedhez!`);
+                    
+                };
+            }
         })
         doboz.appendChild(gomb);
         hely.appendChild(doboz);
     } 
 }
+let vizsg = false;
+function kosar(){
+    if(vizsg==false){
+        document.getElementById("kosar").style.display="block";
+        vizsg=true;
+        kosar_fris();
+    }
+    else{
+        document.getElementById("kosar").style.display="none";
+        vizsg=false;
+    }
 
-function r_hozad(asd){
-    rendeles.append(String(asd));
-    console.log(rendeles);
 }
-
+function kosar_fris(){
+    let ki = document.getElementById("kosar");
+    ki.innerHTML="";
+    szamlalo=0;
+    rendeles.forEach(element => {
+        if(element.darab!=0)ki.innerHTML+=`${element.name} : x${element.darab}</br>`;
+        else szamlalo++;
+    });
+    console.log(szamlalo);
+    if(szamlalo==20)ki.innerHTML="Üres Kosarad!";
+}
 sz();
